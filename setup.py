06c50f257e9d99fd3bacda8633c6b5dbaa167233
nahuelwexd <nahual_gomca@outlook.com.ar>
2018-09-27 02:45:58 -0300

    Added missing ',' to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = '0.6.0',
     packages = ['animuxbot'],
     author = 'nahuelwexd',
-    license = 'GPL-3.0'
+    license = 'GPL-3.0',
     entry_points = {
         'console_scripts': [
             'animuxbot = animuxbot.__main__:main'
