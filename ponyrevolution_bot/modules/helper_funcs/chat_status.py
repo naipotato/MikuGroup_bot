2893d53fd2ad7f8d2763b2c7fca6cffd4013c556
nahuelwexd <nahual_gomca@outlook.com.ar>
2018-11-28 10:05:56 -0300

    Added user_can_restrict() function and refactored bot_can_restrict()

--- ponyrevolution_bot/modules/helper_funcs/chat_status.py
+++ ponyrevolution_bot/modules/helper_funcs/chat_status.py
@@ -1,16 +1,31 @@
 # -*- coding: utf-8 -*-
 from functools import wraps
 
-from telegram import Bot, Update
+from telegram import Bot, Update, ChatMember
 
 
 def bot_can_restrict(func):
     @wraps(func)
     def restrict_rights(bot: Bot, update: Update, *args, **kwargs):
-        if update.effective_chat.get_member(bot.id).can_restrict_members:
+        chat_member = update.effective_chat.get_member(bot.id)
+        if chat_member.can_restrict_members:
             return func(bot, update, *args, **kwargs)
         else:
             update.effective_message.reply_text(
                 'Etto... no tengo los permisos para banear usuarios (╥_╥)')
+
+    return restrict_rights
+
+
+def user_can_restrict(func):
+    @wraps(func)
+    def restrict_rights(bot: Bot, update: Update, *args, **kwargs):
+        chat_member = update.effective_chat.get_member(
+            update.effective_user.id)
+        if chat_member.can_restrict_members or chat_member.status == ChatMember.CREATOR:
+            return func(bot, update, *args, **kwargs)
+        else:
+            update.effective_message.reply_text(
+                'Lo siento, pero me dijeron que tú no puedes banear usuarios...')
     
     return restrict_rights
