2461d1a23340eab6debd139a0d2b7aa83bb43530
Nahuel wexd <nahual_gomca@outlook.com.ar>
2019-06-18 15:38:19 -0300

    Changed indentation to tabs in order to make happy to gofmt

--- bot.go
+++ bot.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "strconv"
+	"log"
+	"net/http"
+	"strconv"
 
-    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 type bot struct {
-    token   string
-    botAPI  *tgbotapi.BotAPI
-    updates tgbotapi.UpdatesChannel
+	token   string
+	botAPI  *tgbotapi.BotAPI
+	updates tgbotapi.UpdatesChannel
 }
 
 func botNew(token string) *bot {
