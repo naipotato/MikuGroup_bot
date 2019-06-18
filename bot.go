fcddebcd0beba03d3d60874191362d02bcc46e86
Nahuel wexd <nahual_gomca@outlook.com.ar>
2019-06-18 15:34:20 -0300

    Implemented run() and listenToCommands()

--- bot.go
+++ bot.go
@@ -30,9 +30,37 @@ func botNew(token string) *bot {
 }
 
 func (bot *bot) run(port int) {
+	_, err := bot.botAPI.SetWebhook(tgbotapi.NewWebhook("https://ponyrevolution-bot.herokuapp.com/" + bot.token))
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	info, err := bot.botAPI.GetWebhookInfo()
+	if err != nil {
+		log.Fatal(err)
+	} else if info.LastErrorDate != 0 {
+		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+	}
+
+	bot.updates = bot.botAPI.ListenForWebhook("/" + bot.token)
+	go http.ListenAndServe("0.0.0.0:"+strconv.Itoa(port), nil)
+
+	bot.listenToCommands()
 }
 
 func (bot *bot) listenToCommands() {
+	for update := range bot.updates {
+		if update.Message != nil {
+			continue
+		}
 
+		if update.Message.Chat.Type == "supergroup" {
+			if !update.Message.IsCommand() {
+				continue
+			}
+
+			switch update.Message.Command() {
+			}
+		}
+	}
 }
