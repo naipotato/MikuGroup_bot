4f14138343e4d9d7c0c68513d6682b202a22113c
Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
2020-12-01 17:24:25 -0300

    Port bot module

---
+++ src/bot.ts
@@ -0,0 +1,33 @@
+import Telegraf from 'telegraf';
+import Ban from './ban';
+import { TOKEN } from './constants';
+import DeleteMessages from './deleteMessages';
+import General from './general';
+import NewMembers from './newMembers';
+
+const bot = new Telegraf(TOKEN);
+
+// Nos aseguramos que el evento realmente tenga un mensaje
+bot.use(Telegraf.filter(ctx => {
+	return ctx.message != undefined;
+}));
+
+// Si el mensaje proviene de un supergrupo, continuamos.
+// En caso contrario, abandonamos el chat si es posible
+bot.use(async (ctx, next) => {
+	const chat = ctx.chat ?? await ctx.getChat();
+
+	if (chat.type == 'supergroup') {
+		await next();
+	} else if (chat.type != 'private') {
+		await ctx.leaveChat();
+	}
+});
+
+// Configuramos el resto de middlewares
+bot.use(NewMembers);
+bot.use(DeleteMessages);
+bot.use(Ban);
+bot.use(General);
+
+bot.launch();
